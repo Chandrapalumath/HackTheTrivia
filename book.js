cc8e3e0b5fde7adfd79498ffc4bdf92676e40670
ShashwatPS <shashwat123student@gmail.com>
2024-01-28 11:40:03 +0530

    Fixed book.js

--- book.js
+++ book.js
@@ -1,39 +1,77 @@
-import pg from "pg";
-const { Client } = pg;
+
 // const queryString = window.location.search;
     // const urlParams = new URLSearchParams(queryString);
     // const buttonValue1 = urlParams.get('buttonValue');
     // var a=document.getElementById('doc');
     // a.innerHTML+=buttonValue1;
 
-    const connectionString = "postgresql://banachandrapal999:WX03zeUDknEG@ep-noisy-heart-a1tpohhd.ap-southeast-1.aws.neon.tech/neondb?sslmode=require";
+import pg from "pg";
+const { Client } = pg;
 
-    const client = new Client({
+const connectionString = "postgresql://banachandrapal999:WX03zeUDknEG@ep-noisy-heart-a1tpohhd.ap-southeast-1.aws.neon.tech/neondb?sslmode=require";
+
+const client = new Client({
     connectionString: connectionString
+});
+
+client.connect();
+
+// Function to create the patientinfo table
+function createTable() {
+    const createTableQuery = `
+        CREATE TABLE IF NOT EXISTS patientinfo (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255),
+            age INTEGER,
+            aadhar VARCHAR(16),
+            mobile VARCHAR(15),
+            address VARCHAR(255),
+            d DATE,
+            reason VARCHAR(255)
+        );
+    `;
+
+    client.query(createTableQuery, (err, res) => {
+        if (err) {
+            console.error("Error creating table:", err);
+        } else {
+            console.log("Table created successfully");
+        }
+    });
+}
+
+createTable();
+
+function submitall() {
+    var name = document.getElementById('name').value;
+    var age = document.getElementById('age').value;
+    var mobile = document.getElementById('mob').value;
+    var add = document.getElementById('Adnum').value;
+    var aad = document.getElementById('add').value;
+    var date = document.getElementById('date').value;
+    var reason = document.getElementById('reason').value;
+    alert(name + mobile + add + aad + date + reason);
+
+    const insertQuery = `
+        INSERT INTO patientinfo (name, age, aadhar, mobile, address, d, reason)
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+    `;
+
+    client.query(insertQuery, [name, age, aad, mobile, add, date, reason], (err, res) => {
+        if (err) {
+            console.error("Error inserting data:", err);
+        } else {
+            console.log("Data inserted successfully");
+        }
     });
 
-    client.connect();
-
-    function submitall(){
-        var name=document.getElementById('name').value;
-        var age=document.getElementById('age').value;
-        var mobile=document.getElementById('mob').value;
-        var add=document.getElementById('Adnum').value;
-        var aad=document.getElementById('add').value;
-        var date=document.getElementById('date').value;
-        var reason=document.getElementById('reason').value;
-        alert(name + mobile + add + aad + date + reason);
-        db.connect();
-
-        db.query("insert into patientinfo {name,age,aadhar,mobile,address,d,reason} values($1,$2,$3,$4,$5,$6,$7)",[name,age,aad,mobile,add,date,reason]);
-     db.query("select * from users ",(err,res)=>{
-        if(err){
-            console.err("hii");
+    const selectQuery = "SELECT * FROM users";
+    client.query(selectQuery, (err, res) => {
+        if (err) {
+            console.error("Error querying users:", err);
+        } else {
+            console.log("User data", res.rows);
         }
-        else{
-            console.log("user data",res.rows);
-        }
-        db.end();
     });
 }
 
