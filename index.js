50a125d60068a7971397ebaafbd2f564b0cf009e
Chandrapal Singh <banachandrapal999@gmail.com>
2024-01-28 18:11:55 +0530

    Removed Unecessary Files

--- index.js
+++
@@ -1,4 +0,0 @@
-function redirectToPage2() {
-    var buttonValue = document.getElementById('d1').value;
-    window.location.href = 'booking.html?buttonValue=' + encodeURIComponent(buttonValue);
-  }
